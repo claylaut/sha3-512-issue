3fdf74119d6b029b194b98a2f451f5a8fbc77756
Clayton Lautier <clayton.lautier@outlook.com>
2020-04-17 20:11:29 +0200

    feat(sha): sample issue

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,107 @@
+﻿using SHA3.Net;
+using System;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    public class HMACChiko2 : KeyedHashAlgorithm
+    {
+        private Sha3 hash1;
+        private Sha3 hash2;
+        private bool bHashing = false;
+        private byte[] rgbInner = new byte[128];
+        private byte[] rgbOuter = new byte[128];
+        public HMACChiko2(byte[] rgbKey)
+        {
+            HashSizeValue = 512;
+            // Create the hash algorithms.
+            hash1 = Sha3.Sha3512();
+            hash2 = Sha3.Sha3512();
+            // Get the key.
+            if (rgbKey.Length > 128)
+            {
+                KeyValue = hash1.ComputeHash(rgbKey);
+                // No need to call Initialize; ComputeHash does it automatically.
+            }
+            else
+            {
+                KeyValue = (byte[])rgbKey.Clone();
+            }
+            // Compute rgbInner and rgbOuter.
+            int i = 0;
+            for (i = 0; i < 128; i++)
+            {
+                rgbInner[i] = 0x36;
+                rgbOuter[i] = 0x5C;
+            }
+            for (i = 0; i < KeyValue.Length; i++)
+            {
+                rgbInner[i] ^= KeyValue[i];
+                rgbOuter[i] ^= KeyValue[i];
+            }
+        }
+        public override byte[] Key
+        {
+            get { return (byte[])KeyValue.Clone(); }
+            set
+            {
+                if (bHashing)
+                {
+                    throw new Exception("Cannot change key during hash operation");
+                }
+                if (value.Length > 128)
+                {
+                    KeyValue = hash1.ComputeHash(value);
+                    // No need to call Initialize; ComputeHash does it automatically.
+                }
+                else
+                {
+                    KeyValue = (byte[])value.Clone();
+                }
+                // Compute rgbInner and rgbOuter.
+                int i = 0;
+                for (i = 0; i < 128; i++)
+                {
+                    rgbInner[i] = 0x36;
+                    rgbOuter[i] = 0x5C;
+                }
+                for (i = 0; i < KeyValue.Length; i++)
+                {
+                    rgbInner[i] ^= KeyValue[i];
+                    rgbOuter[i] ^= KeyValue[i];
+                }
+            }
+        }
+        public override void Initialize()
+        {
+            hash1.Initialize();
+            hash2.Initialize();
+            bHashing = false;
+        }
+        protected override void HashCore(byte[] rgb, int ib, int cb)
+        {
+            if (bHashing == false)
+            {
+                hash1.TransformBlock(rgbInner, 0, 128, rgbInner, 0);
+                bHashing = true;
+            }
+            hash1.TransformBlock(rgb, ib, cb, rgb, ib);
+        }
+        protected override byte[] HashFinal()
+        {
+            if (bHashing == false)
+            {
+                hash1.TransformBlock(rgbInner, 0, 128, rgbInner, 0);
+                bHashing = true;
+            }
+            // Finalize the original hash.
+            hash1.TransformFinalBlock(new byte[0], 0, 0);
+            // Write the outer array.
+            hash2.TransformBlock(rgbOuter, 0, 128, rgbOuter, 0);
+            // Write the inner hash and finalize the hash.
+            hash2.TransformFinalBlock(hash1.Hash, 0, hash1.Hash.Length);
+            bHashing = false;
+            return hash2.Hash;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secretKey = "1234";
+            var valueToEncrypt = "Hello";
+            var encryptedExpectedResult =
+                "8f8a3752ee05d4465f1747918016fe2dd28fc487652efc2d32a8bce90cf8f4e53de87eccd6c0386ec18811ca8cbf37927a47b5266fb16373194be9a9b105f809";
+            var finalExpectedResult =
+                "OGY4YTM3NTJlZTA1ZDQ0NjVmMTc0NzkxODAxNmZlMmRkMjhmYzQ4NzY1MmVmYzJkMzJhOGJjZTkwY2Y4ZjRlNTNkZTg3ZWNjZDZjMDM4NmVjMTg4MTFjYThjYmYzNzkyN2E0N2I1MjY2ZmIxNjM3MzE5NGJlOWE5YjEwNWY4MDk=";
+
+            Console.WriteLine($"input: {valueToEncrypt}");
+
+
+            var encryptedResult = SHA512_ComputeHash(valueToEncrypt, secretKey);
+            Console.WriteLine($"encrypted string: {encryptedResult}");
+            Console.WriteLine($"expected encrypted Result: {encryptedExpectedResult}");
+            Console.WriteLine($"Is Match: {encryptedResult == encryptedExpectedResult}");
+
+
+            Console.WriteLine($"=============================================================================");
+
+            // var actualResult = Base64Encode(encryptedExpectedResult);
+            var actualResult = Base64Encode(encryptedResult);
+
+            Console.WriteLine($"encoded string: {actualResult}");
+            Console.WriteLine($"expected Result: {finalExpectedResult}");
+            Console.WriteLine($"Is Match: {actualResult == finalExpectedResult}");
+            Console.ReadLine();
+        }
+
+        public static string SHA512_ComputeHash(string text, string secretKey)
+        {
+            var hash = new StringBuilder();
+            byte[] secretKeyBytes = Encoding.UTF8.GetBytes(secretKey);
+            byte[] inputBytes = Encoding.UTF8.GetBytes(text);
+            using (var hmac = new HMACSHA512(secretKeyBytes))
+            {
+                byte[] hashValue = hmac.ComputeHash(inputBytes);
+                foreach (var theByte in hashValue)
+                {
+                    hash.Append(theByte.ToString("x2"));
+                }
+            }
+
+            return hash.ToString();
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+    }
+}
